e424b91eff548c5c0226568c1635f2cf5b129d12
1301311267-cyber <1301311267@s.tusd1.org>
2026-03-04 17:58:33 +0000

    done

--- script.js
+++ script.js
@@ -2,21 +2,39 @@ console.log("Script started")
 
 //Variable to track the number of clicks
 let clicks = 0;
+let clickValue = 1;
 
 // Variable to store the click display h1 element
 let clickDisplay = document.getElementById("click-display");
+
+//variable to store the level display element 
+let levelDisplay = document.getElementById("level-display");
 
 // Set the initial text 
 clickDisplay.innerText = "Clicks: " + clicks;
 
 // Called when the potato is clicked 
 function handleClick() {
-    console.log("click");
+    console.log("clickValue");
 
 
-//Add one to click count
-Clicks = clicks + 1;
+    //Count the click
+    clicks = clicks + clickValue;
 
-//Update display 
-clickDisplay.innerText = "Clicks " + clicks;
+    //Update display with new value
+    clickDisplay.innerText = "Clicks: " + clicks;
+
+    //check if we're to another level
+    if (clicks == 10) {
+        alert("Congrants! You've reached level 2");
+        levelDisplay.innerText = "Level 2";
+        clickValue = 2;
+    }
+    else if (clicks ==50) {
+        alert("Congrats! You've reached level 3");
+        levelDisplay.innerText = "Level 3";
+        clickValue = 5;
+    }
+        
 }
+
